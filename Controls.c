b09fc7b68a0f4c6f428c2b7647cb909b7813d85e
Sam Blazes <sblazes16@sidwell.edu>
2016-03-12 14:56:48 -0500

    removed extraneous auto-generated comments and added more useful ones.

--- Controls.c
+++ Controls.c
@@ -37,39 +37,11 @@
 
 void initializeRobot()
 {
-  // Place code here to sinitialize servos to starting positions.
-  // Sensors are automatically configured and setup by ROBOTC. They may need a brief time to stabilize.
+	//Clear Debug stream (persists from last time
+	//the program was run if you don't do this.)
 	clearDebugStream();
 	return;
 }
-
-
-/////////////////////////////////////////////////////////////////////////////////////////////////////
-//
-//                                         Main Task
-//
-// The following is the main code for the tele-op robot operation. Customize as appropriate for
-// your specific robot.
-//
-// Game controller / joystick information is sent periodically (about every 50 milliseconds) from
-// the FMS (Field Management System) to the robot. Most tele-op programs will follow the following
-// logic:
-//   1. Loop forever repeating the following actions:
-//   2. Get the latest game controller / joystick settings that have been received from the PC.
-//   3. Perform appropriate actions based on the joystick + buttons settings. This is usually a
-//      simple action:
-//      *  Joystick values are usually directly translated into power levels for a motor or
-//         position of a servo.
-//      *  Buttons are usually used to start/stop a motor or cause a servo to move to a specific
-//         position.
-//   4. Repeat the loop.
-//
-// Your program needs to continuously loop because you need to continuously respond to changes in
-// the game controller settings.
-//
-// At the end of the tele-op period, the FMS will autonmatically abort (stop) execution of the program.
-//
-/////////////////////////////////////////////////////////////////////////////////////////////////////
 
 void driveForward(int speed) {
 	motor[motorLeft] = speed;
@@ -96,42 +68,41 @@ task main()
 
 	while (true)
 	{
+		//Update joystick state variable
 		getJoystickSettings(joystick);
-		///////////////////////////////////////////////////////////
-		///////////////////////////////////////////////////////////
-		////                                                   ////
-		////      Add your robot specific tele-op code here.   ////
-		////                                                   ////
-		///////////////////////////////////////////////////////////
-		///////////////////////////////////////////////////////////
 
-		// Insert code to have servos and motors respond to joystick and button values.
-
-		// Look in the ROBOTC samples folder for programs that may be similar to what you want to perform.
-		// You may be able to find "snippets" of code that are similar to the functions that you want to
-		// perform.
+		//store values for later use.
 		int left  = joystick.joy1_y1;
 		int right = joystick.joy1_y2;
 
 		/*
+		//Debug joystick positions.
 		char buf[128];
 		sprintf(buf,"Joystick Positions: %8d %8d",left,right);
 
 		writeDebugStreamLine(buf);
 		// */
 
+		//If the left joystick is over a certain threshold,
+		//set the left main propeller motor power to that
+		//value. Otherwise, stop the motor.
 		if (abs(left) > 25) {
 			motor[motorLeft] = -(left-sgn(left)*25);
 		} else {
 			motor[motorLeft] = 0;
 		}
 
+		//If the right joystick is over a certain threshold,
+		//set the right main propeller motor power to that
+		//value. Otherwise, stop the motor.
 		if (abs(right) > 25) {
 			motor[motorRight] = (right-sgn(right)*25);
 		} else {
 			motor[motorRight] = 0;
 		}
 
+		//Open (btn 5) or Close (btn 7) the net grabber.
+		//If neither button is pressed, stop the motor.
 		if (joy1Btn(8)) {
 			motor[motorUpDown] = 50;
 		} else if (joy1Btn(6)) {
@@ -140,6 +111,8 @@ task main()
 			motor[motorUpDown] = 0;
 		}
 
+		//Open (btn 3) or Close (btn 1) the main grabber.
+		//If neither button is pressed, stop the motor.
 		if (joy1Btn(3)) {
 			motor[motorGrab] = 50;
 		} else if (joy1Btn(1)) {
@@ -148,6 +121,8 @@ task main()
 			motor[motorGrab] = 0;
 		}
 
+		//Open (btn 5) or Close (btn 7) the net grabber.
+		//If neither button is pressed, stop the motor.
 		if (joy1Btn(5)) {
 			writeDebugStreamLine("Open");
 			motor[motorNet] = 50;
